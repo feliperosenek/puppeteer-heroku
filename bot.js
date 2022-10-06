8f81845c6b73df999fd965699c093efb17328c0c
Felipe Rosene Klapsztein <feliperosenek@gmail.com>
2022-10-06 11:06:01 -0300

    config

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const puppeteer = require("puppeteer")
-
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 const bot = async () => {
     let options = {
         headless: true,
@@ -9,6 +9,7 @@ const bot = async () => {
     let browser = await puppeteer.launch(options);   
 
     const page = await browser.newPage();
+    await delay(10000)
     await page.goto("http://google.com"),{
         waitUntil: 'networkidle2'
     }
